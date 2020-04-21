adbfa9a0a830f690b38d31b0d9378aa45b10b6c4
Nikos Filinis <nfil245@hotmail.gr>
2020-04-21 03:03:44 +0300

    Add basic info about pcl types

--- src/llvm/ast.hpp
+++ src/llvm/ast.hpp
@@ -5,20 +5,107 @@
 
 using namespace llvm;
 
-class AST {
+extern int line_num;
+
+class Node {
 public:
-    virtual ~AST() {}
+    virtual ~Node() = default;
     virtual Value *codegen() = 0;
 };
 
-class Expr:  public AST {};
-class Stmt:  public AST {};
+/***********************LANGUAGE TYPES***********************/
 
-class Id:    public Expr {};
-class Const: public Expr {};
-class Binop: public Expr {};
-class If:    public Expr {};
-class For:   public Stmt {};
-class Block: public Stmt {};
+/* Name: integer
+ * Size: At least 2 bytes
+ * Info: Two's compliment representation
+ */
+class Integer: public Node {
+    int val;
+public:
+    Value *codegen() override;
+};
+
+/* 
+ * Name: boolean
+ * Size: 1 byte
+ * Info: false(=0) and true(=1)
+ */
+class Boolean: public Node {
+    bool val;
+public:
+    Value *codegen() override;
+};
+
+/* 
+ * Name: char
+ * Size: 1 byte
+ * Info: ASCII representation
+ */
+class Char: public Node {
+    char val;
+public:
+    Value *codegen() override;
+};
+
+/* 
+ * Name: real
+ * Size: 10 bytes
+ * Info: IEEE 754 representation
+ */
+class Real: public Node {
+    double val;
+public:
+    Value *codegen() override;
+};
+
+/* 
+ * Name: array[n] of t
+ * Size: n * sizeof(t) bytes
+ * Info: Increasing order of addresses
+ */
+class Array_n: public Node {
+public:
+    Value *codegen() override;
+};
+
+/* 
+ * Name: array of t
+ * Info: Unknown number of elements
+ */
+class Array: public Node {
+public:
+    Value *codegen() override;
+};
+
+/* 
+ * Name: ^t
+ * Size: 2 bytes
+ * Info: Pointer to valid type t
+ */
+class Pointers: public Node {
+public:
+    Value *codegen() override;
+};
+
+class Var: public Node {
+};
+
+class BinOp: public Node {
+};
+
+class If: public Node {
+};
+
+class For: public Node {
+};
+
+class Block: public Node {
+};
+
+class Function: public Node {
+};
+
+class While: public Node {
+};
 
 #endif
